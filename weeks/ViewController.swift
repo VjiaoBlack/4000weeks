e0d06514a23d879aa236b9d908fad0c4713a9369
WFT <will.a.ft@gmail.com>
2015-09-04 22:54:55 -0400

    Initial Commit

---
+++ weeks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weeks
+//
+//  Created by Will on 9/4/15.
+//  Copyright (c) 2015 Will Field-Thompson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
